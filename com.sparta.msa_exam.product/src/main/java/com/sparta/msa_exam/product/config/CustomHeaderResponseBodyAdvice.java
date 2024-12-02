099fa3e41a8f8607cbcb0379bf11c2763e9359be
JISU-YANG <jisuyang@egoist.im>
2024-12-03 00:28:00 +0900

    feat: 서버 포트를 추가하는 ResponseBodyAdvice 구현

---
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/config/CustomHeaderResponseBodyAdvice.java
@@ -0,0 +1,33 @@
+package com.sparta.msa_exam.product.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@Component
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CustomHeaderResponseBodyAdvice implements ResponseBodyAdvice<Object> {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        response.getHeaders().add("Server-Port", serverPort);
+        return body;
+    }
+}
